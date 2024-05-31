6cfdd1e2a05176bf4e9d87fab18ca68c79ac4260
ivanLFN <LFN.current@yandex.com>
2024-05-31 12:48:55 +0700

    feat(signUp.vue): adding sign up page

---
+++ components/ConfirmComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="confirm-class">Do you want to lose your completed data?</div>
+    <div class="flex justify-start mt-5">
+      <button class="cancel-btn me-5 px-10" @click="cancel">Cancel</button>
+      <button class="confirm-btn px-10" @click="confirm">Accept</button>
+    </div>
+  </template>
+  
+<script setup lang="ts">
+ import { defineEmits } from 'vue'
+
+const emits = defineEmits(['confirmed', 'canceled'])
+
+const confirm = () => {
+  emits('confirmed')
+}
+
+const cancel = () => {
+  emits('canceled')
+}
+</script>
